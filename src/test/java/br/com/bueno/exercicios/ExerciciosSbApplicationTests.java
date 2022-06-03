afe82fe6c081f15b9c1b3408ff038fee471aadaa
bueno <joaopedrobueno2910@gmail.com>
2022-06-02 23:00:23 -0300

    Initial commit

---
+++ src/test/java/br/com/bueno/exercicios/ExerciciosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bueno.exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
